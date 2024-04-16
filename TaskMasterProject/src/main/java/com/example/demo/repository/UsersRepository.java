d33caca1f696775a2467f55830c96e8560c2f5c4
ErtugrulHanBarbaros <115144315+snypr4151@users.noreply.github.com>
2024-04-16 14:03:01 +0300

    Delete TaskMasterProject directory

--- TaskMasterProject/src/main/java/com/example/demo/repository/UsersRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.UsersEntity;
-
-public interface UsersRepository extends JpaRepository<UsersEntity, Long> {
-}
