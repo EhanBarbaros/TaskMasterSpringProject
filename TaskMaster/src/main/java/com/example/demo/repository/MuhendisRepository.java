cada0f62c7cc4fd4a309dd1c69330d8449986380
ErtugrulHanBarbaros <115144315+snypr4151@users.noreply.github.com>
2024-05-10 15:03:25 +0300

    Delete TaskMaster directory

--- TaskMaster/src/main/java/com/example/demo/repository/MuhendisRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.MuhendisEntity;
-
-public interface MuhendisRepository extends JpaRepository<MuhendisEntity, Long> {
-
-}
--- TaskMaster/src/main/java/com/example/demo/repository/TakimLideriRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.TakimLideriEntity;
-
-public interface TakimLideriRepository extends JpaRepository<TakimLideriEntity, Long> {
-
-}
-
